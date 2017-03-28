5cb5ec0d2a77f8fbfb03fb7c62f4443023ba5575
bunge <bunge@ubuntu>
2017-03-28 08:55:17 -0700

    autocompress

--- module/zfs/zfs_ioctl.c
+++ module/zfs/zfs_ioctl.c
@@ -2415,6 +2415,11 @@ zfs_prop_set_special(const char *dsname, zprop_source_t source,
 		if (err == 0)
 			err = -1;
 		break;
+	case ZFS_PROP_COMPRESSION:
+		if (intval == ZIO_COMPRESS_AUTO) {
+			dsl_dataset_activate_compress_auto(dsname);
+		}
+		break;
 	case ZFS_PROP_RESERVATION:
 		err = dsl_dir_set_reservation(dsname, source, intval);
 		break;
@@ -3824,6 +3829,20 @@ zfs_check_settable(const char *dsname, nvpair_t *pair, cred_t *cr)
 
 				if (!spa_feature_is_enabled(spa,
 				    SPA_FEATURE_LZ4_COMPRESS)) {
+					spa_close(spa, FTAG);
+					return (SET_ERROR(ENOTSUP));
+				}
+				spa_close(spa, FTAG);
+			}
+
+			if (intval == ZIO_COMPRESS_AUTO) {
+				spa_t *spa;
+
+				if ((err = spa_open(dsname, &spa, FTAG)) != 0)
+					return (err);
+
+				if (!spa_feature_is_enabled(spa,
+				    SPA_FEATURE_COMPRESS_AUTO)) {
 					spa_close(spa, FTAG);
 					return (SET_ERROR(ENOTSUP));
 				}
